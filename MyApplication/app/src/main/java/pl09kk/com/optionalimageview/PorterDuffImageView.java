08e0d1c5b417ea7b25bfabf3c75b45fa30a80e5d
xiaqibo <bob.xia@yaok.com>
2017-07-17 17:05:00 +0800

    创建

---
+++ MyApplication/app/src/main/java/pl09kk/com/optionalimageview/PorterDuffImageView.java
@@ -0,0 +1,23 @@
+package pl09kk.com.optionalimageview;
+
+import android.content.Context;
+import android.support.v7.widget.AppCompatImageView;
+import android.util.AttributeSet;
+
+/**
+ * Created by xiaqibo on 2017/7/17.
+ */
+
+public class PorterDuffImageView extends AppCompatImageView {
+    public PorterDuffImageView(Context context) {
+        super(context);
+    }
+
+    public PorterDuffImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public PorterDuffImageView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
